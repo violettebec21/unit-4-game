5535c2460ef0b31d55fd9804ca5e7c1b86576e97
violettebec <bechoerner@gmail.com>
2018-12-20 23:05:55 -0600

    adding files with score fix

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,22 @@
+//make sure our js is linked to index.html file
 console.log("sanity")
 
-// Create variables that hold references to the places in the HTML where we want to display things.
+// Create variable for game directions that holds reference to the places in the HTML where we want to display
 var directions = document.getElementById("directions");
 
+// Write code that selects a random number to be shown at the start of the game
+// Number should be should be between 19 - 120
 $(document).ready(function () {
-  var Random = Math.floor(Math.random() * 100)
-  // Selects a random number to be shown at the start of the game
-  // Number should be should be between 19 - 120
+  var randomNumber = Math.floor(Math.random() * 100)
 
 
-  // Appending random number to the randomNumber id in the html doc
-  $('#randomNumber').text(Random);
+  // Append the random number to the randomNumber id in the html doc to display from js to html
+  $('#randomNumber').text(randomNumber);
 
-  //define variables for wins, losses, guesses logged
-
+  //define variables for wins & losses & points 
   var wins = 0;
   var losses = 0;
+  var points = 0; 
 
   //call from html
   $('#numberWins').text(wins);
@@ -24,17 +25,18 @@ $(document).ready(function () {
   //make the jewels clickable buttons
   //this is where we delegate the on.click --> bind to the document, but only listen for on.clicks for gem image buttons
   $(document).on("click", ".gemImage", function () {
-    var points = $(this).val();
+    points += parseInt($(this).val());
+    $('#userScore').text(points);
     console.log(points);
     console.log($(this).val());
   })
 
-  // defined a reusable piece of code, wrapped in a function. these pieces are now in scope
-  // every time we call it, it's like we call it for the first time
+  // define a reusable piece of code, wrapped in a function. these pieces are now in scope
+  // every time we call this function, it's like we call for the first time
   function renderButtons() {
-    //defined an array so we could cycle through the array --> data structuring
+    //define an array so we can cycle through the array --> data structuring
     var gemImages = ["assets/images/blue.jpg", "assets/images/green.jpg", "assets/images/purple.jpg", "assets/images/red.jpg"];
-    
+
     //THIS IS WHERE WE CYCLE THROUGH THE DATA
     for (var i = 0; i < gemImages.length; i++) {
       //37-40: dynamically created our img tag, set its attr (attributes)
@@ -50,6 +52,30 @@ $(document).ready(function () {
   //this is where we call our function
   renderButtons();
 
+  //need to display user total score for the value that the crystal button is worth + display in html
+  var userScore = 0;
 
+  //need to connect gem button press values to userScore variable, append each number guess to score 
+
+  //need to define reset function
+
+
+
+  //write IF statement to determine wins & losses
+  if (userScore === randomNumber) {
+    wins++;
+    $("#result").html("Winner!");
+  }
+
+  else if (userScore > randomNumber) {
+    losses++;
+    $("#result").html("Sorry, you lose!");
+  }
 
 })
+
+//this is where we call our function to reset game when completed
+
+};
+
+
